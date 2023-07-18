5a6953795be856ae05fa953ee07ab7fc06b162de
dayroncj <dayroncj@gmail.com>
2023-07-17 21:19:37 -0500

    Added description

--- dbo/Tables/Tablea.sql
+++ dbo/Tables/Tablea.sql
@@ -11,3 +11,12 @@ GO
 
 GO
 
+
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'Test',
+    @level0type = N'SCHEMA',
+    @level0name = N'dbo',
+    @level1type = N'TABLE',
+    @level1name = N'Tablea',
+    @level2type = N'COLUMN',
+    @level2name = N'colc'
